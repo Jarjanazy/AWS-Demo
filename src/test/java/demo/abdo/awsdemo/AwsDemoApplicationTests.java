3421994b7aa77b4743ebbcf033aca7c394285056
cercenazi <celil.cercenazi@gmail.com>
2021-09-09 11:22:58 +0300

    adh the initial project

---
+++ src/test/java/demo/abdo/awsdemo/AwsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.abdo.awsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
